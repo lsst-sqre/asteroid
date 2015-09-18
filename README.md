220363fc92b98ef7330eef39a2e434e819ff1273
J. Matt Peterson <jmatt@jmatt.org>
2015-09-17 19:53:49 -0700

    Initial demonstration on programmatically using nebula.
    	new file:   .gitignore
    	new file:   LICENSE
    	new file:   README.md
    	new file:   asteroid/__init__.py
    	new file:   asteroid/deploy.py
    	new file:   requirements.txt

---
+++ README.md
@@ -0,0 +1,9 @@
+asteroid
+========
+
+A simple demonstration of how to use the python [libcloud](http://libcloud.readthedocs.org/en/latest/index.html) library with NCSA's nebula cloud.
+
+License
+-------
+
+See the LICENSE file.
---
+++ asteroid/deploy.py
@@ -0,0 +1,45 @@
+import getpass
+from sys import stdin, stdout
+
+from libcloud.compute.types import Provider
+from libcloud.compute.providers import get_driver as libcloud_get_driver
+from libcloud.security import VERIFY_SSL_CERT
+
+
+VERIFY_SSL_CERT = False
+
+
+def get_driver(username, password):
+    OpenStack = libcloud_get_driver(Provider.OPENSTACK)
+    return OpenStack(
+        key=username,
+        secret=password,
+        ex_tenant_name='LSST',
+        ex_force_auth_url='http://nebula.ncsa.illinois.edu:5000/v2.0/tokens/',
+        ex_force_auth_version='2.0_password')    
+
+
+def prompt_username():
+    username = getpass.getuser()
+    print('Username [%s] ⇝ ' % (username), end='', flush=True)
+    _input = stdin.readline()
+    if _input.strip():
+        return _input.strip()
+    else:
+        return username
+    
+
+def prompt_and_get_driver():
+    return get_driver(prompt_username(),
+                      getpass.getpass(prompt='Password ⇝ '))
+
+
+def main():
+    driver = prompt_and_get_driver()
+    print(driver.list_sizes())
+    print(driver.list_images())
+    print(driver.list_nodes())
+
+
+if __name__ == '__main__':
+    main()
